170ecc8b7ad97e94921631eee34b5b7c35525142
Alexander Kermani <alexander.kermani@gmail.com>
2025-05-12 12:17:31 -0500

    added firebase components

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,28 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCSmaOHLFGfW7mxtaKLvvSAhCsXmXGLils",
+  authDomain: "kaiser-electrical-services.firebaseapp.com",
+  projectId: "kaiser-electrical-services",
+  storageBucket: "kaiser-electrical-services.firebasestorage.app",
+  messagingSenderId: "744798509005",
+  appId: "1:744798509005:web:508ce5c23c78a330c05684",
+  measurementId: "G-Y416FDBFX1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
