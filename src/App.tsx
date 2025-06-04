9b8a76f63a44f26dda2ab5201e335f3948a07e65
Alexander Kermani <alexander.kermani@gmail.com>
2025-06-03 20:16:22 -0500

    fixed logo link

--- src/App.tsx
+++ src/App.tsx
@@ -14,27 +14,27 @@ function App() {
   return (
     <>
 
-      <h3> <u>KAISER ELECTRICAL SERVICES</u> </h3><br/><br/>
+      <h3> <u>KAISER ELECTRICAL SERVICES</u> </h3><br /><br />
       <h1> SITE UNDER CONSTRUCTION </h1>
 
       <p>
-        New management is handling the construction of our website. <br/>Thank you for your interest and your patience.
+        New management is handling the construction of our website. <br />Thank you for your interest and your patience.
       </p>
 
       <div>
-        <a href="https://vite.dev" target="_blank">
+        <a href="https://kaiserelectricalservices.com" target="_blank">
           <img src={KESLogo} className="logo" alt="KES logo" />
         </a>
-        <br/><br/>
+        <br /><br />
       </div>
 
       <div>
         <p className="read-the-docs">
-          Please reach us by...<br/><br/>
-          
-             Phone: <a href="tel:(936)933-3604">(936)933-3604</a><br/>
-             Email: <a href="mailto:c.kaiser@kaiserelectricalservices.com">c.kaiser@kaiserelectricalservices.com</a><br/>
-             Alt Email: <a href="mailto:m.reeves@kaiserelectricalservices.com">m.reeves@kaiserelectricalservices.com</a><br/>
+          Please reach us by...<br /><br />
+
+          Phone: <a href="tel:(936)933-3604">(936)933-3604</a><br />
+          Email: <a href="mailto:c.kaiser@kaiserelectricalservices.com">c.kaiser@kaiserelectricalservices.com</a><br />
+          Alt Email: <a href="mailto:m.reeves@kaiserelectricalservices.com">m.reeves@kaiserelectricalservices.com</a><br />
         </p>
       </div>
     </>
